f3805c630e9895729ccb91fb95f4d647f38de3c0
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-08-24 18:41:29 -0400

    [PRESENTATION] Fixed bug where passing :api does not suffice

--- lib/intermodal/concerns/models/presentation.rb
+++ lib/intermodal/concerns/models/presentation.rb
@@ -4,14 +4,16 @@ module Intermodal
       extend ActiveSupport::Concern
 
       def as_json(opts = {})
-        return presenter(opts) if opts && (opts[:presenter] || opts[:api])
+        return presentation(opts) if opts && (opts[:presenter] || opts[:api])
         super(opts)
       end
 
       def presenter(opts)
-        raise "Must pass :presenter" unless opts[:presenter]
-        presenter = opts[:presenter] || opts[:api].presenter_for(self.class)
-        presenter.call(self, opts[:scope] || :default)
+        opts[:presenter] || opts[:api].presenter_for(self.class)
+      end
+
+      def presentation(opts)
+        presenter(opts).call(self, opts[:scope] || :default)
       end
     end
   end
