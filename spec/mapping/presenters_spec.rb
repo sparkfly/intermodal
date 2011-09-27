1d2f57fc3bf902fd1bff2fd357bcb8ed6da8d627
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-09-27 17:40:06 -0400

    [SPEC] Intermodal::Mapping::Presenter.call with nested mappings should present merged fields

--- spec/mapping/presenters_spec.rb
+++ spec/mapping/presenters_spec.rb
@@ -29,6 +29,38 @@ describe Intermodal::Mapping::Presenter do
         subject[:description].should be_nil
       end
     end
+
+    context 'with nested mappings' do
+      let(:presenter) do
+        define_class :TestPresenter, Intermodal::Mapping::Presenter do
+
+          # Use this to create nested presentation
+          presents :metadata, :as => [ :name, :description ]
+        end
+      end
+
+      let(:resource) { Item.make(:name => name, :description => description) }
+      let(:name) { 'Weird Eureka Thing' }
+      let(:description) { 'Makes Eureka Go Boom!' }
+
+      it 'should merge fields into a single hash of fields' do
+        should include(:metadata)
+        subject[:metadata].should include(:name)
+        subject[:metadata].should include(:description)
+      end
+
+      it 'should merge fields without transforming its value' do
+        subject[:metadata][:name].should eql(name)
+        subject[:metadata][:description].should eql(description)
+      end
+
+      # Unless you've explicitly declared the merged fields, it should
+      # not present it.
+      it 'should not present undeclared fields' do
+        should_not include(:name)
+        should_not include(:description)
+      end
+    end
   end
 end
 
