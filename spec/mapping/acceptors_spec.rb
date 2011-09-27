809b8db87318da5977d3525c9cfa8efca876e48b
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-09-27 15:38:54 -0400

    [SPEC] Wrapped existing examples under the context, "with pass-through mappings"

--- spec/mapping/acceptors_spec.rb
+++ spec/mapping/acceptors_spec.rb
@@ -6,55 +6,57 @@ describe Intermodal::Mapping::Acceptor do
   describe '.call' do
     subject { acceptor.call(params) }
 
-    let(:acceptor) do
-      define_class :TestAcceptor, Intermodal::Mapping::Acceptor do
-        accepts :acceptable_field
-        accepts :optional_field
+    context 'with pass-through mappings' do
+      let(:acceptor) do
+        define_class :TestAcceptor, Intermodal::Mapping::Acceptor do
+          accepts :acceptable_field
+          accepts :optional_field
+        end
       end
-    end
 
-    let(:value) { 'Accepted Field' }
-    let(:params) { { :acceptable_field => value, :unacceptable_field => true } }
+      let(:value) { 'Accepted Field' }
+      let(:params) { { :acceptable_field => value, :unacceptable_field => true } }
 
-    it 'should accept explicitly whitelisted fields' do
-      should include(:acceptable_field)
-    end
-
-    it 'should pass through value without transformation' do
-      subject[:acceptable_field].should eql(value)
-    end
-
-    it 'should not accept undeclared fields' do
-      should_not include(:unacceptable_field)
-    end
-
-    context 'without optional field in params' do
-      # Since we pass the output of an acceptor directly into
-      # the active record #create or #update_attributes, we want
-      # the hash to look like:
-      #
-      # { :acceptable_fields => 'Something' }
-      #
-      # rather than
-      #
-      # { :acceptable_field => 'Something', :optional_field => nil }
-      #
-      # Doing so will update the resource and null out the fields that
-      # have not been passed through params.
-      it 'should not include the optional field' do
-        should_not include(:optional_field)
-      end
-    end
-
-    context 'with optional field in params' do
-      let(:params) { { :acceptable_field => value, :optional_field => value } }
-
-      it 'should include the optional field' do
-        should include(:optional_field)
+      it 'should accept explicitly whitelisted fields' do
+        should include(:acceptable_field)
       end
 
       it 'should pass through value without transformation' do
-        subject[:optional_field].should eql(value)
+        subject[:acceptable_field].should eql(value)
+      end
+
+      it 'should not accept undeclared fields' do
+        should_not include(:unacceptable_field)
+      end
+
+      context 'without optional field in params' do
+        # Since we pass the output of an acceptor directly into
+        # the active record #create or #update_attributes, we want
+        # the hash to look like:
+        #
+        # { :acceptable_fields => 'Something' }
+        #
+        # rather than
+        #
+        # { :acceptable_field => 'Something', :optional_field => nil }
+        #
+        # Doing so will update the resource and null out the fields that
+        # have not been passed through params.
+        it 'should not include the optional field' do
+          should_not include(:optional_field)
+        end
+      end
+
+      context 'with optional field in params' do
+        let(:params) { { :acceptable_field => value, :optional_field => value } }
+
+        it 'should include the optional field' do
+          should include(:optional_field)
+        end
+
+        it 'should pass through value without transformation' do
+          subject[:optional_field].should eql(value)
+        end
       end
     end
 
