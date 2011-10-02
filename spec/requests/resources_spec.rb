6bc4f1c301bc69a395149828413056153f2e20c9
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-10-02 11:07:27 -0400

    [SPEC] Factored out stubbing/initializing to a spec helpers

--- spec/requests/resources_spec.rb
+++ spec/requests/resources_spec.rb
@@ -4,40 +4,8 @@ require 'tsort'
 describe Intermodal::ResourceController do
   include Intermodal::RSpec::Resources
   include SpecHelpers::Application
+  include SpecHelpers::API
 
-  let(:application) { api }
-
-  let(:http_headers) { {'X-Auth-Token' => access_token.token } }
-
-  # TODO: REFACTOR
-  # This has to be done because we're using a custom serializer to Redis
-  # The right thing to do is to use Warden's serialization. This allows
-  # people to use their own authentication scheme besides the built-in one
-  # (and possibly spin off the built-in auth as its own gem).
-  let(:stub_warden_serializer) do
-    class Warden::SessionSerializer
-      def store(user, scope)
-      end
-
-      def fetch(scope)
-      end
-
-      def stored?(scope)
-        false
-      end
-
-      def delete(scope, user=nil)
-      end
-    end
-  end
-  let(:stub_abstract_store) do
-    class ::ActionDispatch::Session::AbstractStore
-      def destroy_session(env, sid, options) 
-      end
-    end
-  end
-
-  let(:skipped_initializers) { [:add_routing_paths, :append_assets_path, :prepend_helpers_path] }
   let(:api_class) do
     define_class :Api, Rails::Engine do
       include Intermodal::API
@@ -63,16 +31,6 @@ describe Intermodal::ResourceController do
       routes.draw do
         resources :items
       end
-    end
-  end
-
-  let(:api) do
-    api_class.tap do |a|
-      stub_warden_serializer
-      stub_abstract_store
-      a.instance.initializers.reject! { |initializer| skipped_initializers.include? initializer.name }
-
-      a.instance.initialize!
     end
   end
 
