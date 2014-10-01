30627ca3973ac153eb1ada312cb556da24b2dbd1
Ho-Sheng Hsiao <talktohosh@gmail.com>
2014-10-01 15:45:52 -0700

    Put UseWarden and Rails3Stack into Intermodal::Concerns

--- lib/intermodal/concerns/rails/rails_3_stack.rb
+++ lib/intermodal/concerns/rails/rails_3_stack.rb
@@ -1,39 +1,42 @@
 require 'intermodal/rack/rescue'
 
 module Intermodal
-  module Rails
-    module Rails3Stack
-      # Override the middleware stack to something more streamlined
+  module Concerns
+    module Rails
+      module Rails3Stack
+        extend ActiveSupport::Concern
+        # Override the middleware stack to something more streamlined
 
-      included do
-        def default_middleware_stack
-          ActionDispatch::MiddlewareStack.new.tap do |middleware|
-            #middleware.use ::Rack::Lock unless config.allow_concurrency
-            middleware.use ::Rack::Runtime
-            middleware.use ::Intermodal::Rack::Rescue
-            middleware.use ::Rails::Rack::Logger
-            middleware.use ::ActionDispatch::RemoteIp, config.action_dispatch.ip_spoofing_check, config.action_dispatch.trusted_proxies
-            middleware.use ::Rack::Sendfile, config.action_dispatch.x_sendfile_header
-            middleware.use ::ActionDispatch::Callbacks #, !config.cache_classes
+        included do
+          def default_middleware_stack
+            ActionDispatch::MiddlewareStack.new.tap do |middleware|
+              #middleware.use ::Rack::Lock unless config.allow_concurrency
+              middleware.use ::Rack::Runtime
+              middleware.use ::Intermodal::Rack::Rescue
+              middleware.use ::Rails::Rack::Logger
+              middleware.use ::ActionDispatch::RemoteIp, config.action_dispatch.ip_spoofing_check, config.action_dispatch.trusted_proxies
+              middleware.use ::Rack::Sendfile, config.action_dispatch.x_sendfile_header
+              middleware.use ::ActionDispatch::Callbacks #, !config.cache_classes
 
-            if config.session_store
-              middleware.use config.session_store, config.session_options
-              middleware.use ::ActionDispatch::Flash
+              if config.session_store
+                middleware.use config.session_store, config.session_options
+                middleware.use ::ActionDispatch::Flash
+              end
+
+              middleware.use ::ActionDispatch::ParamsParser
+              middleware.use ::Rack::MethodOverride
+              middleware.use ::ActionDispatch::Head
             end
+          end
 
-            middleware.use ::ActionDispatch::ParamsParser
-            middleware.use ::Rack::MethodOverride
-            middleware.use ::ActionDispatch::Head
+          # Query caching doesn't work well with what we are doing
+          # Maybe this has changed since Rails 4.
+          initializer 'intermodal_example.hack_active_record', :after => 'eager_load!' do
+            config.middleware.delete ActiveRecord::QueryCache
           end
         end
 
-        # Query caching doesn't work well with what we are doing
-        # Maybe this has changed since Rails 4.
-        initializer 'intermodal_example.hack_active_record', :after => 'eager_load!' do
-          config.middleware.delete ActiveRecord::QueryCache
-        end
       end
-
     end
   end
 end
--- lib/intermodal/concerns/rails/use_warden.rb
+++ lib/intermodal/concerns/rails/use_warden.rb
@@ -2,15 +2,17 @@ require 'intermodal/rack/auth'
 
 # Use this to configure Warden to use X_AUTH_TOKEN strategy
 module Intermodal
-  module Rails
-    module UseWarden
-      extend ActiveSupport::Concern
+  module Concerns
+    module Rails
+      module UseWarden
+        extend ActiveSupport::Concern
 
-      included do
-        config.middleware.use Warden::Manager do |manager|
-          manager.default_strategies :x_auth_token #, :basic
-          manager.failure_app = proc do
-            Intermodal::Rack::Auth::UNAUTHORIZED
+        included do
+          config.middleware.use Warden::Manager do |manager|
+            manager.default_strategies :x_auth_token #, :basic
+            manager.failure_app = proc do
+              Intermodal::Rack::Auth::UNAUTHORIZED
+            end
           end
         end
       end
